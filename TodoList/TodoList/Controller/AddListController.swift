7328bd61d4adcd8247e2e91ab1cbf53d4eb2b3f5
Deeadline <mikszym95@gmail.com>
2018-06-12 14:20:55 +0200

    Update AddListController.swift

--- TodoList/TodoList/Controller/AddListController.swift
+++ TodoList/TodoList/Controller/AddListController.swift
@@ -10,11 +10,6 @@ import UIKit
 
 class AddListController: UIViewController {
 
-    //Polecam kumplu robic coś takiego ala segregowanie kodu, co mam na myśli?
-    
-    
-    //tutaj zmienne i referencje
-    // MARK: Referencje
     @IBOutlet weak var textField: UITextField!
     
     @IBAction func addList(_ sender: UIButton) {
@@ -25,29 +20,11 @@ class AddListController: UIViewController {
         
     }
     
-    //tutaj funkcje takie ogolne z dziedziczenia. to jest spoko to wszystko wiecej nie ruszam
-    //spoko pozdrawiam :D dzieki za rady :D
-    // MARK: Konstruktor
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    //tutaj eventy
-    
-    
-    
-    //tutaj funkcje prywatne, polecam funckej prywatne pisac w stylu
-    
-    fileprivate func funkcja() {
-        // bo to jest dość spoko
-    }
-    
-
+    }  
 }
